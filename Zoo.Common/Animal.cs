c69969b72998174a976b9b4314bd8f5b97d2366a
Jonas Fagerberg <info@csharpschool.com>
2023-09-18 15:30:51 +0200

    Add project files.

---
+++ Zoo.Common/Animal.cs
@@ -0,0 +1,12 @@
+﻿namespace Zoo.Common;
+
+public struct Animal
+{
+    public string AnimalType { get; init; }
+    public bool Predator { get; private set; }
+
+    public Animal(string animalType, bool predator) => 
+        (AnimalType, Predator) = (animalType, predator);
+
+   public void Change(bool predator) => Predator = predator;
+}
---
+++ Zoo.Data/ZooData.cs
@@ -0,0 +1,16 @@
+﻿using Zoo.Common;
+
+namespace Zoo.Data;
+
+public class ZooData
+{
+    private Animal[] Animals { get; init; } = new[] {
+        new Animal("Lion", true),
+        new Animal("Giraffe", false),
+        new Animal("Quagga", false),
+        new Animal("Tiger", true)
+    };
+
+    public Animal[] GetAnimals(bool p) { 
+        return Animals.Where(a => a.Predator == p).ToArray(); }
+}
